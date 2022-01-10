4db69bdc1dab680d08941d39891c35e44271b8e2
MananSodhi <msodhi_be19@thapar.edu>
2022-01-11 00:31:12 +0530

    Time and Space Complexity Analysis

---
+++ 2. Time and Space Complexity Analysis/05 Max_product_subarray.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int Max_Product_Subarray(int arr[], int n){
+    int max_ending_here = 1;
+    int min_ending_here = 1;
+    int max_so_far = 0;
+
+    for (int i = 0; i < n; i++){
+        if (arr[i] > 0){
+            max_ending_here = max_ending_here * arr[i];
+            min_ending_here = min(1, min_ending_here * arr[i]);
+        }
+        else if(arr[i] < 0){
+            int temp = max_ending_here;
+            max_ending_here = max(1, min_ending_here * arr[i]);
+            min_ending_here = temp * arr[i];
+        }
+        else{
+            max_ending_here = 1;
+            min_ending_here = 1;
+        }
+        
+        max_so_far = max(max_so_far, max_ending_here);
+    }
+
+    return max_so_far; 
+}
+
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    cout << Max_Product_Subarray(arr, n) << endl;
+}
---
+++ 2. Time and Space Complexity Analysis/06 Mind_bending_product_explain.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void Max_Product(int arr[], int n){
+    long * p = new long int[n];
+    long lp =1;
+    for (int i = 0; i < n; i++){
+        p[i] = lp;
+        lp = lp * arr[i];
+    }
+
+
+    long rp = 1;
+    for (int i = n-1; i >= 0; i--){
+        p[i] = p[i] * rp;
+        rp = rp * arr[i]; 
+    }
+
+    for (int i = 0; i < n; i++){
+        cout << p[i] <<" ";
+    }
+    cout << endl;
+}
+
+ int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    #endif    
+     int t;
+     cin >> t;
+
+     while (t--){
+         int n;
+         cin >> n;
+         int arr[n];
+         for (int i = 0; i < n; i++){
+             cin >> arr[i];
+         }
+        Max_Product(arr, n);
+     }
+     return 0;
+ }
