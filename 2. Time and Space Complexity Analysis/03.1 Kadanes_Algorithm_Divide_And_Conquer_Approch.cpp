3aede8c8c36b080bba2c57403bb1c4e3fbc2fa39
MananSodhi <msodhi_be19@thapar.edu>
2022-01-23 14:03:52 +0530

    Divide_And_Conquer_Approch

--- 2. Time and Space Complexity Analysis/03.1 Kadanes_Algorithm_Divide_And_Conquer_Approch.cpp
+++ 2. Time and Space Complexity Analysis/03.1 Kadanes_Algorithm_Divide_And_Conquer_Approch.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 //DIVIDE AND CONQUER APPROCH
+//Time O(nLogn)
 // 1. divide array in 2 halfes
 // 2. return the max of following
 // 	2.1 Max subarray sum in left half (make recursive call)
@@ -19,10 +20,10 @@ int max(int a, int b, int c) {
 	return (max(max(a, b), c));
 }
 
-int maxCrossingSum(int arr[], iint l, int m, int h) {
+int maxCrossingSum(int arr[], int l, int m, int h) {
 	int sum = 0;
 	int left_sum = INT_MIN;
-	for (int i = m; i > l; i--) {
+	for (int i = m; i >= l; i--) {
 		sum += arr[i];
 		if (sum > left_sum) {
 			left_sum = sum;
@@ -31,7 +32,7 @@ int maxCrossingSum(int arr[], iint l, int m, int h) {
 
 	sum = 0;
 	int right_sum = INT_MIN;
-	for (int i = m + 1; i < h; i++) {
+	for (int i = m + 1; i <= h; i++) {
 		sum += arr[i];
 		if (right_sum < sum) {
 			right_sum = sum;
@@ -46,6 +47,13 @@ int kadane(int * arr, int l, int h) {
 		return arr[l];
 	}
 	int m = (l + h) / 2;
+
+    /* Return maximum of following three possible cases
+            a) Maximum subarray sum in left half
+            b) Maximum subarray sum in right half
+            c) Maximum subarray sum such that the subarray
+       crosses the midpoint */
+
 	return max(maxSubArraySum(arr, l, m), maxSubArraySum(arr, m + 1, h), maxCrossingSum(arr, l, m, h));
 }
 
@@ -54,14 +62,15 @@ int main() {
 	freopen("input.txt", "r", stdin);
 	freopen("output.txt", "w", stdout);
 #endif
-	int lower,upper = 0;
-	cin >> lower;
-	int * arr = new int [lower];
-	for (int i = 0; i < lower; i++) {
+	int lower = 0;
+	int upper ;
+	cin >> upper;
+	int * arr = new int [upper];
+	for (int i = 0; i < upper; i++) {
 		cin >> arr[i];
 	}
 
-	cout << kadane(arr, lower, upper) << endl;
+	cout << maxSubArraySum(arr, lower, upper) << endl;
 
 
 	return 0
