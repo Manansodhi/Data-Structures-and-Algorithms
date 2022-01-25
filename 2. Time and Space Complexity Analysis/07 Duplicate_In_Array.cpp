9132ca8757b2a0fae97c682a3051ba67d489f2bc
MananSodhi <msodhi_be19@thapar.edu>
2022-01-25 07:12:11 +0530

    Practice_Problems

--- 2. Time and Space Complexity Analysis/07 Duplicate_In_Array.cpp
+++ 2. Time and Space Complexity Analysis/07 Duplicate_In_Array.cpp
@@ -1,10 +1,14 @@
 /* Duplicate in array
 
-You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging from 0 to 3, and among these, there is a single integer value that is present twice. You need to find and return that duplicate number present in the array.
+You have been given an integer array/list(ARR) of size N which contains numbers from 0 to (N - 2). 
+Each number is present at least once. That is, if N = 5, the array/list constitutes values ranging 
+from 0 to 3, and among these, there is a single integer value that is present twice. You need to find
+ and return that duplicate number present in the array.
 Note : Duplicate number is always present in the given array/list.
 
 Input format :
-The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. 
+Then the test cases follow.
 First line of each test case or query contains an integer 'N' representing the size of the array/list.
 Second line contains 'N' single space separated integers representing the elements in the array/list.
 
@@ -50,12 +54,14 @@ using namespace std;
   Space :- O(1)
 
 2. Sorting Approch
-   sort the array in O(log n) and then compare two consective elements if they are same return that it will take O(n)
+   sort the array in O(log n) and then compare two consective elements if they are same return that it
+   will take O(n)
    TIME :- O(log n)
    Space :- O(1)
 
 3. Optimized Approch
-   find sum of all elements of array in O(n) time and then also find the sum of first (n-2) natural numbers becoz we know that we have elements of array btw 0 - (n-2) it will take O(1)time direct formula   
+   find sum of all elements of array in O(n) time and then also find the sum of first (n-2) natural numbers 
+   becoz we know that we have elements of array btw 0 - (n-2) it will take O(1)time direct formula   
    TIME :- O(n)
    Space :- O(1)
 
