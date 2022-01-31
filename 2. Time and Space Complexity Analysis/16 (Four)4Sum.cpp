39215046270912981d6fcd852738cd1a642a67b6
MananSodhi <msodhi_be19@thapar.edu>
2022-01-31 12:11:18 +0530

    Practice_Problems

--- 2. Time and Space Complexity Analysis/16 (Four)4Sum.cpp
+++ 2. Time and Space Complexity Analysis/16 (Four)4Sum.cpp
@@ -64,6 +64,69 @@ vector<vector<int>> FourSum(vector<int>& nums, int target) {
 }
 
 
+/*
+Optimized Approach
+Time Complexity: O(N³)
+Space Complexity: O(1)
+Over here we will fix two-pointers and then find the remaining two elements using two pointer technique as the array will be 
+sorted at first
+*/
+
+
+vector<vector<int>> fourSum(vector<int>& num, int target){
+	vector<vector<int>> res;
+
+	if(num.empty()){
+		return res;
+	}
+
+	int n = num.size();
+	sort(num.begin(),num.end());
+
+	for(int i = 0; i<n;i++){
+		for(int j =i+1;j<n;j++){
+			int target_2 = target - num[i]-num[j];
+
+			int left = j+1;
+			int right = n-1;
+
+			while(left<right){
+				int two_sum = num[left]+num[right];
+				if(two_sum < target_2){
+					left++;
+				}
+				else if(two_sum >target_2){
+					right--;
+				}
+				else{
+					vector<int> quadruplet(4,0);
+					quadruplet[0] = num[i];
+					quadruplet[1] = num[j];
+					quadruplet[2] = num[left];
+					quadruplet[3] = num[right];
+					res.emplace_back(quadruplet);
+
+
+					while(left<right && num[left] == quadruplet[2])
+						++left;
+
+
+					while(left<right && num[left] == quadruplet[3])
+						--right;
+
+				}
+			}
+
+			while(j+1 < n && num[j+1] == num[j])
+				++j;
+		}
+		while(i+1<n && num[i+1] == num[i])
+			++i;
+	}
+
+	return res;
+
+}
 
 
 
