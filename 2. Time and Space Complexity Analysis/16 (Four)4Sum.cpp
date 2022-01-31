5f0b1867976aac41e506f478ed475a4521c2917f
MananSodhi <msodhi_be19@thapar.edu>
2022-01-31 11:29:38 +0530

    Practice_Problems

--- 2. Time and Space Complexity Analysis/16 (Four)4Sum.cpp
+++ 2. Time and Space Complexity Analysis/16 (Four)4Sum.cpp
@@ -28,6 +28,43 @@ Constraints:
 #include<bits/stdc++.h>
 using namespace std;
 
+/*
+Using 3 pointers and Binary Search
+Time Complexity: O(N log N + N³ logN)
+Reason: Sorting the array takes N log N and three nested loops will be taking N³ and for binary search, it takes another log N.
+Space Complexity: O(M * 4), where M is the number of quads
+**/
+
+vector<vector<int>> FourSum(vector<int>& nums, int target) {
+	int n =  nums.size();
+	sort(nums.begin(), nums.end());
+
+	set<vector<int>> sv;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = i + 1; j < n; j++) {
+			for (int k = j + 1; k < n; k++) {
+				int x = (long long) target - (long long) nums[i] - (long long) nums[j] - (long long) nums[k];
+
+				if(binary_search(nums.begin()+k+1,nums.end(),x)){
+					vector<int> v;
+					v.emplace_back(nums[i]);
+					v.emplace_back(nums[j]);
+					v.emplace_back(nums[k]);
+					v.emplace_back(x);
+					sort(v.begin(),v.end());
+					sv.insert(v);
+				}
+			}
+		}
+	}
+
+	vector<vector<int>> res(sv.begin(),sv.end());
+	return res;
+}
+
+
+
 
 
 int main() {
@@ -39,17 +76,20 @@ int main() {
 	cin >> t;
 
 	while (t--) {
-		int arr[size];
+		vector<int>v;
 		for (int i = 0; i < size; i++) {
 			cin >> arr[i];
 		}
 		int target;
 		cin >> target;
 
-		vector<int> ans = TwoSum(arr, target);
-		for (auto i = ans.begin(); i != ans.end(); ++i) {
-			cout << *i << " ";
+		vector<vector<int>> ans = FourSum(arr, target);
+		for (int i = 0; i < ans.size(); i++) {
+			for (int j = 0; j < ans[i].size(); j++)
+				cout << ans[i][j] << " ";
+			cout << endl;
 		}
+
 		cout << endl;
 		delete[] arr;
 	}
