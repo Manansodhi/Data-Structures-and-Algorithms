8a28d5ab7605b76cb4877ef6950e954abffb8251
MananSodhi <msodhi_be19@thapar.edu>
2022-01-31 09:52:32 +0530

    Practice_Problems

--- 2. Time and Space Complexity Analysis/15 (Two)2Sum.cpp
+++ 2. Time and Space Complexity Analysis/15 (Two)2Sum.cpp
@@ -32,6 +32,59 @@ Only one valid answer exists.
 using namespace std;
 
 
+/*
+ Naive Approach (Brute Force)
+ Time Complexity: O(N2)
+ Space Complexity: O(1)
+*/
+
+vector<int> TwoSum(vector<int>& nums, int target) {
+    vector<int> res;
+    for (int i = 0; i < nums.size(); ++i) {
+   	 for (int j = i + 1; j < nums.size(); ++j) {
+   		 if (nums[i] + nums[j] == target) {
+   			 res.emplace_back(i);
+   			 res.emplace_back(j);
+   			 break;
+   		 }
+   	 }
+   	 if (res.size() == 2)
+   		 break;
+    }
+    return res;
+}
+
+
+/*
+Hashing (Most efficient)
+Time Complexity: O(N)
+Space Complexity: O(N)
+*/
+
+
+vector<int> TwoSum(vector<int>& nums, int target) {
+
+    vector<int> res;
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); ++i) {
+
+   	 if (mp.find(target - nums[i]) != mp.end()) {
+
+   		 res.emplace_back(i);
+   		 res.emplace_back(mp[target - nums[i]]);
+   		 return res;
+   	 }
+
+   	 mp[nums[i]] = i;
+    }
+
+    return res;
+}
+
+
+
+
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
@@ -41,14 +94,14 @@ int main() {
 	cin >> t;
 
 	while (t--) {
-		int size;
-		cin >> size;
 		int arr[size];
 		for (int i = 0; i < size; i++) {
 			cin >> arr[i];
 		}
+		int target;
+		cin >> target;
 
-		vector<int> ans = Longest_Consecutive_Sequence(arr, size);
+		vector<int> ans = TwoSum(arr, target);
 		for (auto i = ans.begin(); i != ans.end(); ++i) {
 			cout << *i << " ";
 		}
