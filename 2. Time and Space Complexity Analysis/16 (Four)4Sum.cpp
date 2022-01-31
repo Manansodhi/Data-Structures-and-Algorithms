4a02e0c7eea73515577f969df3207b5a6e06d33e
MananSodhi <msodhi_be19@thapar.edu>
2022-01-31 10:08:53 +0530

    Practice_Problems

---
+++ 2. Time and Space Complexity Analysis/16 (Four)4Sum.cpp
@@ -0,0 +1,57 @@
+/*
+Given an array nums of n integers, return an array of all the unique quadruplets [nums[a], nums[b], nums[c], nums[d]] such that:
+
+0 <= a, b, c, d < n
+a, b, c, and d are distinct.
+nums[a] + nums[b] + nums[c] + nums[d] == target
+You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,0,-1,0,-2,2], target = 0
+Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Example 2:
+
+Input: nums = [2,2,2,2,2], target = 8
+Output: [[2,2,2,2]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 200
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	int t;
+	cin >> t;
+
+	while (t--) {
+		int arr[size];
+		for (int i = 0; i < size; i++) {
+			cin >> arr[i];
+		}
+		int target;
+		cin >> target;
+
+		vector<int> ans = TwoSum(arr, target);
+		for (auto i = ans.begin(); i != ans.end(); ++i) {
+			cout << *i << " ";
+		}
+		cout << endl;
+		delete[] arr;
+	}
+	return 0;
+}
