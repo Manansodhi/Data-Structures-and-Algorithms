e02220eeb2655a3a2a74864d359bad8742e6c846
MananSodhi <msodhi_be19@thapar.edu>
2022-02-02 23:56:09 +0530

    Sliding_Window_Problem

--- 2. Time and Space Complexity Analysis/19 Longest_Substring_Without_Repeating_Characters.cpp
+++ 2. Time and Space Complexity Analysis/19 Longest_Substring_Without_Repeating_Characters.cpp
@@ -115,7 +115,29 @@ int LongestSubstringWithoutRepeatingCharacters(string s) {
 	return len;
 }
 
-
+//Easy To Understand Code
+int LongestSubstringWithoutRepeatingCharacters(string s){
+	if(s.size()==0){
+		return 0;
+	}
+	int n = s.size();
+	int i(0),j(0);
+	 vector<int> cnt (256,0);
+	 cnt[s[0]]++;
+	 int len = 1;
+	 while(j!=n-1){
+	 	if(cnt[s[j+1]] == 0){
+	 		j++;
+	 		cnt[s[j]]++;
+	 		len = max(len,j-i+1);
+	 	}
+	 	else{
+	 		cnt[s[i]]--;
+	 		i++;	
+	 	}
+	 }
+	 return len;
+}
 
 
 int main() {
