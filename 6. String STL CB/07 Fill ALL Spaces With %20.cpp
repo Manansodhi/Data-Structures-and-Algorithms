a6cd615430a4be3f86fdfaec81180c462469cf8c
MananSodhi <msodhi_be19@thapar.edu>
2022-03-04 01:00:05 +0530

    ALL Spaces With %20
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

---
+++ 6. String STL CB/07 Fill ALL Spaces With %20.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+void space20(char* s) {
+	int countSpace = 0, additionalSpace = 0;
+	for (int itr = 0; s[itr] != '\0'; itr++) {
+		if (s[itr] == ' ') {
+			countSpace++;
+		}
+	}
+	additionalSpace = (3 * countSpace) - countSpace;
+	s[strlen(s) + additionalSpace] = '\0';
+	for (int itr = strlen(s) - 1 + additionalSpace, j = strlen(s) - 1; j >= 0; j--) {
+		if (s[j] == ' ') {
+			s[itr] = '0';
+			s[itr - 1] = '2';
+			s[itr - 2] = '%';
+			itr -= 3;
+		}
+		else {
+			s[itr] = s[j];
+			itr--;
+		}
+	}
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	char s[1000];
+	cin.getline(s, 1000);
+	space20(s);
+	cout << s;
+
+	return 0;
+}
