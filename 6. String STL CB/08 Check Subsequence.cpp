baea64ae04af26630c894329af503b81b1103dc4
MananSodhi <msodhi_be19@thapar.edu>
2022-03-04 01:55:52 +0530

    2-Pointer Approch
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

---
+++ 6. String STL CB/08 Check Subsequence.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+INPUT :- 
+NOTE-> No spaces in between
+codingminutes   length=M
+cines			length=N
+
+OUTPUT:-
+yes
+
+TIME:- O(M+N)
+SPACE:- O(1)
+
+2-Pointer Approch
+
+*/
+
+bool checkSubsequence(string s1,string s2){
+	int m = s1.length(),n=s2.length();
+	int j=n;
+	for(int i=m;i>=0;i--){
+		if(s1[i] == s2[j]){
+			j--;
+		}
+	}
+	if(j==-1){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	string s1,s2;
+	cin>>s1>>s2;
+	cout<<checkSubsequence(s1,s2)<<endl;
+
+	return 0;
+}
