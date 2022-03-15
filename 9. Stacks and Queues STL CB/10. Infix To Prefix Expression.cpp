04bc36d0a6adeb3fc6a7d4db8aa78e1cf9fd5ed8
MananSodhi <msodhi_be19@thapar.edu>
2022-03-15 08:23:54 +0530

    Infix To Prefix Expression
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 9. Stacks and Queues STL CB/10. Infix To Prefix Expression.cpp
+++ 9. Stacks and Queues STL CB/10. Infix To Prefix Expression.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int precedence(char c){
-	if(c=='^')
+int precedence(char c) {
+	if (c == '^')
 		return 3;
-	else if(c=='*' || c=='/')
+	else if (c == '*' || c == '/')
 		return 2;
-	else if(c=='+' || c=='-')
+	else if (c == '+' || c == '-')
 		return 1;
 	else
 		return -1;
@@ -14,37 +14,37 @@ int precedence(char c){
 }
 
 
-string infixToprefix(string s){
+string infixToprefix(string s) {
 	stack<char>st;
 	string res;
-	reverse(s.begin(),s.end());
-	for(int i=0;i<s.length();i++){
-		if((s[i]>='a' && s[i]<='z') || (s[i]>='A' && s[i]<='Z')){
-			res+=s[i];
+	reverse(s.begin(), s.end());
+	for (int i = 0; i < s.length(); i++) {
+		if ((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) {
+			res += s[i];
 		}
-		else if(s[i]==')'){
+		else if (s[i] == ')') {
 			st.push(s[i]);
 		}
-		else if(s[i] == '('){
-			while(!st.empty() && st.top()!=')'){
-				res+=st.top();
+		else if (s[i] == '(') {
+			while (!st.empty() && st.top() != ')') {
+				res += st.top();
 				st.pop();
 			}
 			//for remaining opening bracket
-			if(!st.empty()){
+			if (!st.empty()) {
 				st.pop();
-			} 
+			}
 		}
-		else{
-			while(!st.empty() && precedence(st.top())>precedence(s[i])){
-				res+=st.top();
+		else {
+			while (!st.empty() && precedence(st.top()) > precedence(s[i])) {
+				res += st.top();
 				st.pop();
 			}
 			st.push(s[i]);
 		}
 	}
-	while(!st.empty()){
-		res+=st.top();
+	while (!st.empty()) {
+		res += st.top();
 		st.pop();
 	}
 	return res;
@@ -57,8 +57,8 @@ int main() {
 	freopen("output.txt", "w", stdout);
 #endif
 
-	cout <<infixToprefix("(a-b/c)*(a/k-l)") ;
-	cout<<endl;
+	cout << infixToprefix("(a-b/c)*(a/k-l)") ;
+	cout << endl;
 
 	return 0;
 
