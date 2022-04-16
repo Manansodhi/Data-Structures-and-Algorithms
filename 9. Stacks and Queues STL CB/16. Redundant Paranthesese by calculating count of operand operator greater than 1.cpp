2f72f583257de4e5faabbf50def07954de022ee5
MananSodhi <msodhi_be19@thapar.edu>
2022-04-17 03:03:25 +0530

    Redundant Parenthesis count no opernad+operator btw ()
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 9. Stacks and Queues STL CB/16. Redundant Paranthesese by calculating count of operand operator greater than 1.cpp
+++ 9. Stacks and Queues STL CB/16. Redundant Paranthesese by calculating count of operand operator greater than 1.cpp
@@ -1,24 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
     string s;
     cin >> s;
     bool ans = false;
     stack<char> st;
-    for (int i = 0; i < s.size(); i++){
-        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*'){
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*') {
             st.push(s[i]);
         }
-        else if ( s[i] == '('){
+        else if ( s[i] == '(') {
             st.push(s[i]); //pushing all Operand and Operator in stack except clasing bracket"("
         }
-        else if (s[i] == ')'){
-            if (st.top() == '('){
-                ans = true; 
+        else if (s[i] == ')') {
+            if (st.top() == '(') {
+                ans = true;
                 // ye uske liye jub '(' and ')' eske bich me kuch na ho toh matlab redundant h tu ye true kar deka ans ko
             }
-            while (st.top() == '+'|| st.top() == '/'|| st.top() == '*'|| st.top() == '-'){
+            while (st.top() == '+' || st.top() == '/' || st.top() == '*' || st.top() == '-') {
                 st.pop();
             }
         }
