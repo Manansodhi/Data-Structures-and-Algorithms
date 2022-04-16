0b00e7cebb5a1bf7a6f11d525268033473ff6c8e
MananSodhi <msodhi_be19@thapar.edu>
2022-04-17 02:32:12 +0530

    Redundant Parenthesis count no opernad+operator btw ()
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

---
+++ 9. Stacks and Queues STL CB/16. Redundant Paranthesese by calculating count of operand operator greater than 1.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin >> s;
+    bool ans = false;
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++){
+        if (s[i] == '+' || s[i] == '-' || s[i] == '/' || s[i] == '*'){
+            st.push(s[i]);
+        }
+        else if ( s[i] == '('){
+            st.push(s[i]); //pushing all Operand and Operator in stack except clasing bracket"("
+        }
+        else if (s[i] == ')'){
+            if (st.top() == '('){
+                ans = true; 
+                // ye uske liye jub '(' and ')' eske bich me kuch na ho toh matlab redundant h tu ye true kar deka ans ko
+            }
+            while (st.top() == '+'|| st.top() == '/'|| st.top() == '*'|| st.top() == '-'){
+                st.pop();
+            }
+        }
+        st.pop();//ye sirf '(' esko pop karne ki liye h agar multiple opening paranthessis ho toh
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
