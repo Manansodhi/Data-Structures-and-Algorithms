fe02229d93ae4febe517d41ee6ce8859f5457339
MananSodhi <msodhi_be19@thapar.edu>
2022-04-17 02:34:20 +0530

    Practice STL CB
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- practice/practice_STL_CB_3.0.cpp
+++ practice/practice_STL_CB_3.0.cpp
@@ -45,6 +45,28 @@ using namespace std;
 // }
 
 
+int longeestValidParantheses(string s){
+	stack<int>st;
+	st.push(-1);
+	int max_length=0;
+	for(int i=0;i<s.length();i++){
+		if(s[i]=='('){
+			st.push(i);
+		}
+		else{
+			st.pop();
+			if(st.empty()){
+				st.push(i);
+			}
+			else{
+				max_length=max(max_length,i-st.top());
+			}
+		}
+	}
+	return max_length;
+}
+
+
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
@@ -62,7 +84,9 @@ int main() {
 	// 	cout << "Not Balanced!" << endl;
 	// }
 
-
+	string s = "())";
+	
+	cout << longeestValidParantheses(s) << endl;
 
 	return 0;
 
