c3deb3285372ab761985a4455f9ffdfd28559e70
MananSodhi <msodhi_be19@thapar.edu>
2022-04-17 03:06:09 +0530

    Practice STL CB
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- practice/practice_STL_CB_3.0.cpp
+++ practice/practice_STL_CB_3.0.cpp
@@ -45,26 +45,26 @@ using namespace std;
 // }
 
 
-int longeestValidParantheses(string s){
-	stack<int>st;
-	st.push(-1);
-	int max_length=0;
-	for(int i=0;i<s.length();i++){
-		if(s[i]=='('){
-			st.push(i);
-		}
-		else{
-			st.pop();
-			if(st.empty()){
-				st.push(i);
-			}
-			else{
-				max_length=max(max_length,i-st.top());
-			}
-		}
-	}
-	return max_length;
-}
+// int longeestValidParantheses(string s){
+// 	stack<int>st;
+// 	st.push(-1);
+// 	int max_length=0;
+// 	for(int i=0;i<s.length();i++){
+// 		if(s[i]=='('){
+// 			st.push(i);
+// 		}
+// 		else{
+// 			st.pop();
+// 			if(st.empty()){
+// 				st.push(i);
+// 			}
+// 			else{
+// 				max_length=max(max_length,i-st.top());
+// 			}
+// 		}
+// 	}
+// 	return max_length;
+// }
 
 
 int main() {
@@ -84,9 +84,43 @@ int main() {
 	// 	cout << "Not Balanced!" << endl;
 	// }
 
-	string s = "())";
+	// string s = "())";
 	
-	cout << longeestValidParantheses(s) << endl;
+	// cout << longeestValidParantheses(s) << endl;
+
+
+	queue<char>q;
+	int freq[27] = {0};
+
+	char ch;
+	cin>>ch;
+
+	while(ch!= '.'){
+		//reading stream of character untill "."
+		q.push(ch);
+		freq[ch-'a']++; 
+		//storing frequency of each alphabet at its same position in english language like c-a=2 so freq of c will be 
+		//stored at 2nd index by incrementing each time
+
+		//query -> Now removing all chars from front of queue which have frequency greater than 1
+
+		while(!q.empty()){
+			int idx=q.front()-'a';  
+			//finding index of alphabet by subtracting from 'a' to check the freq of that character
+			if(freq[idx]>1){
+				q.pop();
+			}
+			else{
+				cout<<q.front()<<endl;
+				break;
+			}
+		}
+		if(q.empty()){
+			cout<<"-1"<<endl;
+		}
+		cin>>ch;//reading next character
+	}//end of while loop
+
 
 	return 0;
 
