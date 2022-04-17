29445c6603d0a1e9dac1199d1d5813335da85424
MananSodhi <msodhi_be19@thapar.edu>
2022-04-18 01:55:03 +0530

    Practice STL CB
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- practice/practice_STL_CB_3.0.cpp
+++ practice/practice_STL_CB_3.0.cpp
@@ -67,6 +67,55 @@ using namespace std;
 // }
 
 
+string SimplifyPath(string path){
+	istringstream iss(path);
+
+	vector<string> tokens;
+	string token;
+	while(getline(iss, token, '/')){
+		if(token =="." or token==""){
+			continue;
+		}
+		tokens.push_back(token);
+	}
+
+	vector<string> stack;
+	if(path[0]=='/'){
+		stack.push_back("");
+	}
+
+	for(string token:tokens){
+		if(token == ".."){
+			if(stack.size()==0 or stack[stack.size()-1]==".."){
+				stack.push_back("..");
+			}
+			else if(stack[stack.size()-1] != ""){
+				stack.pop_back();
+			}
+		}
+		else{
+			stack.push_back(token);
+		}
+	}
+
+	//corner case when we have only single element
+	if(stack.size()==1 and stack[0]==""){
+		return "/";
+	}
+
+	ostringstream oss;
+	int i=0;
+	for(auto token:stack){
+		if(i!=0){
+			oss<< "/";
+		}i++;
+		oss<<token;
+	}
+
+	return oss.str();
+
+}
+
 int main() {
 #ifndef ONLINE_JUDGE
 	freopen("input.txt", "r", stdin);
@@ -89,38 +138,41 @@ int main() {
 	// cout << longeestValidParantheses(s) << endl;
 
 
-	queue<char>q;
-	int freq[27] = {0};
+	// queue<char>q;
+	// int freq[27] = {0};
 
-	char ch;
-	cin>>ch;
+	// char ch;
+	// cin>>ch;
 
-	while(ch!= '.'){
-		//reading stream of character untill "."
-		q.push(ch);
-		freq[ch-'a']++; 
-		//storing frequency of each alphabet at its same position in english language like c-a=2 so freq of c will be 
-		//stored at 2nd index by incrementing each time
+	// while(ch!= '.'){
+	// 	//reading stream of character untill "."
+	// 	q.push(ch);
+	// 	freq[ch-'a']++; 
+	// 	//storing frequency of each alphabet at its same position in english language like c-a=2 so freq of c will be 
+	// 	//stored at 2nd index by incrementing each time
 
-		//query -> Now removing all chars from front of queue which have frequency greater than 1
+	// 	//query -> Now removing all chars from front of queue which have frequency greater than 1
 
-		while(!q.empty()){
-			int idx=q.front()-'a';  
-			//finding index of alphabet by subtracting from 'a' to check the freq of that character
-			if(freq[idx]>1){
-				q.pop();
-			}
-			else{
-				cout<<q.front()<<endl;
-				break;
-			}
-		}
-		if(q.empty()){
-			cout<<"-1"<<endl;
-		}
-		cin>>ch;//reading next character
-	}//end of while loop
+	// 	while(!q.empty()){
+	// 		int idx=q.front()-'a';  
+	// 		//finding index of alphabet by subtracting from 'a' to check the freq of that character
+	// 		if(freq[idx]>1){
+	// 			q.pop();
+	// 		}
+	// 		else{
+	// 			cout<<q.front()<<endl;
+	// 			break;
+	// 		}
+	// 	}
+	// 	if(q.empty()){
+	// 		cout<<"-1"<<endl;
+	// 	}
+	// 	cin>>ch;//reading next character
+	// }//end of while loop
 
+    string path = "/../x/y/../z/././w/a///../../c/./";
+
+    cout << SimplifyPath(path) << endl;
 
 	return 0;
 
