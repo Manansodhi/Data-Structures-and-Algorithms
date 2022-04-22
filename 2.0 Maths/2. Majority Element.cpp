c119812ee9ed393b9cd08170e099fcc0dcda5f0b
MananSodhi <msodhi_be19@thapar.edu>
2022-04-22 23:46:57 +0530

    Boyre Moore algorithm
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

---
+++ 2.0 Maths/2. Majority Element.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+      int candidate=0,count=0;
+      for (int num:nums){
+        if(count==0){
+          candidate=num;
+        }
+        if(candidate==num){
+          count++;
+        }
+        else{
+          count--;
+        }
+      }
+      return candidate;
+    }
+};
