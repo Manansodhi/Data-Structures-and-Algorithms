edfc0ee1498d5cfe2ae7145b6adc0722549b2efa
MananSodhi <msodhi_be19@thapar.edu>
2022-04-24 23:55:26 +0530

    BinarySearch + Array + Matrix
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

---
+++ 2.0 Arrays, Sorting, Searching, Maths/6. Search a 2D Matrix.cpp
@@ -0,0 +1,45 @@
+/*
+ Search a 2D Matrix
+Write an efficient algorithm that searches for a value target in an m x n integer matrix matrix. This matrix has the following properties:
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+ 
+Example 1:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3
+Output: true
+
+Example 2:
+Input: matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 13
+Output: false
+ 
+Constraints:
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 100
+-104 <= matrix[i][j], target <= 104
+*/
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+      if(!matrix.size())
+        return false;
+      
+      int n = matrix.size(), m = matrix[0].size();
+      int low=0, high=(n*m)-1;
+      while(low<=high){
+        int mid=(low+(high-low)/2);
+        if(matrix[mid/m][mid%m] == target){
+          return true;
+        }
+        if(matrix[mid/m][mid%m] < target){
+          low=mid+1;
+        }
+        else{
+          high = mid-1;
+        }
+      }
+      return false;
+    }
+};
