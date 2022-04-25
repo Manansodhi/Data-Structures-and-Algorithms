0ec5639d4ead0d1f56c8398eacbbf5d32bf9cc21
MananSodhi <msodhi_be19@thapar.edu>
2022-04-26 04:46:54 +0530

    V.Imp Ques Array + Recursion
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 2.0 Arrays, Sorting, Searching, Maths/11. Return Pair To Sum Up The Taget.cpp
+++ 2.0 Arrays, Sorting, Searching, Maths/11. Return Pair To Sum Up The Taget.cpp
@@ -19,7 +19,7 @@ Output: [[2,2,2,2],[2,3,3],[3,5]]
 Example 3:
 Input: candidates = [2], target = 1
 Output: []
- 
+
 Constraints:
 1 <= candidates.length <= 30
 1 <= candidates[i] <= 200
@@ -35,26 +35,26 @@ vector<int> arr = {};
 // returning all possible choices to make target sum by using suffix of array [curIndex, ...]
 vector<choice> getAllChoices(int curIndex, int target) {
     // base case
-    if(target < 0) return {}; // no valid choice
-    if(target == 0) return {{}}; // one choice, and you chose nothing
-    if(curIndex == arr.size()) return {};
-    
+    if (target < 0) return {}; // no valid choice
+    if (target == 0) return {{}}; // one choice, and you chose nothing
+    if (curIndex == arr.size()) return {};
+
     int curNumber = arr[curIndex];
-    
-    vector<choice> ans = getAllChoices(curIndex+1, target); // curNumber is not used at all
-    
+
+    vector<choice> ans = getAllChoices(curIndex + 1, target); // curNumber is not used at all
+
     vector<choice> other = getAllChoices(curIndex, target - curNumber); // using it once
-    for(choice c: other) {
+    for (choice c : other) {
         c.push_back(curNumber);
-        // now c is a valid choice 
+        // now c is a valid choice
         ans.push_back(c);
     }
-    
+
     return ans;
 }
 
 class Solution {
-    
+
 public:
     vector<choice> combinationSum(vector<int>& candidates, int target) {
         arr = candidates;
