edc79517957da4d7e404fdec2cda247f56563b52
MananSodhi <msodhi_be19@thapar.edu>
2022-04-26 13:44:13 +0530

    VV.Imp Deque Application
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 2.0 Arrays, Sorting, Searching, Maths/12. Number of Recent Calls.cpp
+++ 2.0 Arrays, Sorting, Searching, Maths/12. Number of Recent Calls.cpp
@@ -7,7 +7,7 @@ RecentCounter() Initializes the counter with zero recent requests.
 int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
 It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
 
- 
+
 Example 1:
 Input
 ["RecentCounter", "ping", "ping", "ping", "ping"]
@@ -21,7 +21,7 @@ recentCounter.ping(1);     // requests = [1], range is [-2999,1], return 1
 recentCounter.ping(100);   // requests = [1, 100], range is [-2900,100], return 2
 recentCounter.ping(3001);  // requests = [1, 100, 3001], range is [1,3001], return 3
 recentCounter.ping(3002);  // requests = [1, 100, 3001, 3002], range is [2,3002], return 3
- 
+
 Constraints:
 1 <= t <= 109
 Each test case will call ping with strictly increasing values of t.
@@ -35,12 +35,12 @@ class RecentCounter {
     deque<int> req;
 public:
     RecentCounter() {
-        req = {};    
+        req = {};
     }
-    
+
     int ping(int t) {
         req.push_back(t);
-        while(req.front() < t-3000) req.pop_front();
+        while (req.front() < t - 3000) req.pop_front();
         return req.size();
     }
 };
