bb0de6cd4b56a9e31bfe5aed8df92f07202b7627
MananSodhi <msodhi_be19@thapar.edu>
2022-04-27 22:26:20 +0530

    VV.Imp Deque Application
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 2.0 Arrays, Sorting, Searching, Maths/12. Number of Recent Calls.cpp
+++ 2.0 Arrays, Sorting, Searching, Maths/12. Number of Recent Calls.cpp
@@ -7,7 +7,6 @@ RecentCounter() Initializes the counter with zero recent requests.
 int ping(int t) Adds a new request at time t, where t represents some time in milliseconds, and returns the number of requests that has happened in the past 3000 milliseconds (including the new request). Specifically, return the number of requests that have happened in the inclusive range [t - 3000, t].
 It is guaranteed that every call to ping uses a strictly larger value of t than the previous call.
 
-
 Example 1:
 Input
 ["RecentCounter", "ping", "ping", "ping", "ping"]
