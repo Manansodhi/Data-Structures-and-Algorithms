8f1e4356883211aaefef8525aa38777c57725910
MananSodhi <msodhi_be19@thapar.edu>
2022-04-28 16:25:55 +0530

    V.IMP S.C- O(1) Time-O(2(NxM))
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 2.0 Arrays, Sorting, Searching, Maths/14. Set Matrix Zeroes.cpp
+++ 2.0 Arrays, Sorting, Searching, Maths/14. Set Matrix Zeroes.cpp
@@ -11,13 +11,13 @@ Output: [[1,0,1],[0,0,0],[1,0,1]]
 Example 2:
 Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
- 
+
 Constraints:
 m == matrix.length
 n == matrix[0].length
 1 <= m, n <= 200
 -231 <= matrix[i][j] <= 231 - 1
- 
+
 Follow up:
 
 A straightforward solution using O(mn) space is probably a bad idea.
@@ -28,31 +28,31 @@ Could you devise a constant space solution?
 
 class Solution {
 public:
-    void setZeroes(vector<vector<int>>& matrix) {
-      int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
-  for (int i = 0; i < rows; i++) {
-    //checking if 0 is present in the 0th column or not
-    if (matrix[i][0] == 0) col0 = 0;
-    for (int j = 1; j < cols; j++) {
-      if (matrix[i][j] == 0) {
+  void setZeroes(vector<vector<int>>& matrix) {
+    int col0 = 1, rows = matrix.size(), cols = matrix[0].size();
+    for (int i = 0; i < rows; i++) {
+      //checking if 0 is present in the 0th column or not
+      if (matrix[i][0] == 0) col0 = 0;
+      for (int j = 1; j < cols; j++) {
+        if (matrix[i][j] == 0) {
+          matrix[i][0] = 0;
+          matrix[0][j] = 0;
+        }
+      }
+    }
+    //traversing in the reverse direction and
+    //checking if the row or col has 0 or not
+    //and setting values of matrix accordingly.
+    for (int i = rows - 1; i >= 0; i--) {
+      for (int j = cols - 1; j >= 1; j--) {
+        if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+          matrix[i][j] = 0;
+        }
+      }
+      if (col0 == 0) {
         matrix[i][0] = 0;
-        matrix[0][j] = 0;
       }
-    }
-  }
-  //traversing in the reverse direction and
-  //checking if the row or col has 0 or not
-  //and setting values of matrix accordingly.
-  for (int i = rows - 1; i >= 0; i--) {
-    for (int j = cols - 1; j >= 1; j--) {
-      if (matrix[i][0] == 0 || matrix[0][j] == 0) {
-        matrix[i][j] = 0;
-      }
-    }
-    if (col0 == 0) {
-      matrix[i][0] = 0;
-    }
 
-  }
     }
+  }
 };
