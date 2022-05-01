22a33e2bfb41bfc58c5d3d7302f01ef0ff87dbd6
MananSodhi <msodhi_be19@thapar.edu>
2022-05-01 19:44:31 +0530

    .
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- practice/practice_STL_CB_3.0.cpp
+++ practice/practice_STL_CB_3.0.cpp
@@ -335,48 +335,87 @@ int main() {
 	// 	cin>>d;
 	// }
 
-	int k;
-	cin >> k;
-	vector<vector<int> > a(k);
-	for (int i = 0; i < k; i++) {
-		int size;//size of each array
-		cin >> size;
+	// int k;
+	// cin >> k;
+	// vector<vector<int> > a(k);
+	// for (int i = 0; i < k; i++) {
+	// 	int size;//size of each array
+	// 	cin >> size;
 
-		a[i] = vector<int>(size);
-		for (int j = 0; j < size; j++) {
-			cin >> a[i][j];
-		}
+	// 	a[i] = vector<int>(size);
+	// 	for (int j = 0; j < size; j++) {
+	// 		cin >> a[i][j];
+	// 	}
+	// }
+
+	// vector<int>idx(k, 0);
+	// priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> >pq;
+	// //ye starting m har array se phele phele element utha liya
+	// for (int i = 0; i < k; i++) {
+	// 	pair<int,int>p;
+	// 	p.first=a[i][0];
+	// 	p.second=i;
+	// 	pq.push(p);
+	// }
+	// vector<int> ans;
+	// while (!pq.empty()) {
+	// 	pair<int, int> x = pq.top();
+	// 	pq.pop();
+	// 	ans.push_back(x.first);
+	// 	if (idx[x.second] + 1 < a[x.second].size()) {
+	// 		//ye wala voh case h jab abhi array m element bache hue agar ulta hota toh matlab index wale array
+	// 		//me voh array size bada index store kiya hua jisper ja nahi sakte
+	// 		 pair<int,int>p;
+ // 			 p.first=a[x.second][idx[x.second] + 1];
+ // 			 p.second=x.second;
+	// 		pq.push(p);
+	// 	}
+	// 	idx[x.second] += 1;
+	// }
+
+	// for (int i = 0; i < ans.size(); i++) {
+	// 	cout << ans[i] << endl;
+	// }
+	// cout << endl;
+
+
+
+	unordered_map<string,int>ourMap;
+
+	//insert
+	pair<string,int>p ("abc", 1);
+	ourMap.insert(p);
+
+	ourMap["def"] = 2;
+
+	//find or Access
+
+	cout<<ourMap["abc"]<<endl;
+	cout<<ourMap.at("abc")<<endl;
+	cout<<ourMap["def"]<<endl;
+
+	cout<<"Size: " <<ourMap.size()<<endl;
+	//This will show error
+	//cout<<ourMap.at("ghi")<<endl;
+	cout<<ourMap["ghi"]<<endl;
+	cout<<"Size: " <<ourMap.size()<<endl;
+
+
+	//check presence
+	if(ourMap.count("ghi")>0){
+		cout<<"ghi is present" << endl;
 	}
 
-	vector<int>idx(k, 0);
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> >pq;
-	//ye starting m har array se phele phele element utha liya
-	for (int i = 0; i < k; i++) {
-		pair<int,int>p;
-		p.first=a[i][0];
-		p.second=i;
-		pq.push(p);
+	//erase
+	ourMap.erase("ghi");
+	cout<<"Size: " <<ourMap.size()<<endl;
+	if(ourMap.count("ghi")>0){
+		cout<<"ghi is present" << endl;
 	}
-	vector<int> ans;
-	while (!pq.empty()) {
-		pair<int, int> x = pq.top();
-		pq.pop();
-		ans.push_back(x.first);
-		if (idx[x.second] + 1 < a[x.second].size()) {
-			//ye wala voh case h jab abhi array m element bache hue agar ulta hota toh matlab index wale array
-			//me voh array size bada index store kiya hua jisper ja nahi sakte
-			 pair<int,int>p;
- 			 p.first=a[x.second][idx[x.second] + 1];
- 			 p.second=x.second;
-			pq.push(p);
-		}
-		idx[x.second] += 1;
+	else{
+		cout<<"ghi is being erased"<<endl;
 	}
-
-	for (int i = 0; i < ans.size(); i++) {
-		cout << ans[i] << endl;
-	}
-	cout << endl;
+	
 
 	return 0;
 
