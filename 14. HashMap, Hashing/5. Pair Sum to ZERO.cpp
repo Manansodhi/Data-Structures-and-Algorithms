8ea9bdefc3c584a317a82a8ab6ee6e822189cff8
MananSodhi <msodhi_be19@thapar.edu>
2022-05-02 17:21:03 +0530

    V.V Imp Practice problem
    Signed-off-by: MananSodhi <msodhi_be19@thapar.edu>

--- 14. HashMap, Hashing/5. Pair Sum to ZERO.cpp
+++ 14. HashMap, Hashing/5. Pair Sum to ZERO.cpp
@@ -32,21 +32,30 @@ search for a number which sum gives 0 with every element of array
 /*
 2. Approch Optimize
 Time :- O(N logN)
+Space:- O(logN)
 Here we store the frequency of each element in map which will take O(log N) time and then we will traverse the whole array
  once to find the element from map which when sum with array element give 0 sum and reduce the frequency from map
 */
 
-/*3. Using HashMap
+/*
+3. Using HashMap
 Time :- O(N)
 Space :- O(N)
 In this we are traversing whole array once and we are making pair and maintaing the frequency of each element in hashmap both
  at same time
 */
 
+/*
+4. By Computing Prefix Sum
+-> Map prefix sum to a map
+-> for every key, choose 2 values from all the ocuurences of particular prefix sum(mC2 = m(m-1)/2 )
+-> special case: for prefix sum=0, we have to also include them
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
-// 1 APPROCH
+// 1 Optimize APPROCH
 
 int Pair_Sum_To_Zero(int *arr, int size) {
 	unordered_map<int, int>freq;
@@ -61,7 +70,7 @@ int Pair_Sum_To_Zero(int *arr, int size) {
 	return pairCount;
 }
 
-// 2 APPROCH
+// 2 Optimize APPROCH
 void Pair_Sum_To_Zero(int * arr, int size) {
 	unordered_map<int, int>m1;
 	for (int i = 0; i < size; i++) {
@@ -89,7 +98,7 @@ void Pair_Sum_To_Zero(int * arr, int size) {
 }
 
 
-// 3 APPROCH
+// 3 Optimize APPROCH
 void Pair_Sum_To_Zero(int * arr, int size) {
 	sort(arr, arr + size);
 	int starting_position_of_positive_elements = 0;
@@ -111,6 +120,29 @@ void Pair_Sum_To_Zero(int * arr, int size) {
 }
 
 
+// 4 Optimize APPROCH
+
+int Pair_Sum_To_Zero(int *arr, int size) {
+	map<int, int> cnt;
+	int prefixSum = 0;
+
+	for (int i = 0; i < n; i++) {
+		prefixSum += arr[i];
+		cnt[prefixSum]++;
+	}
+
+	int ans = 0;
+	map<int, int>::iterator it;
+	for (it = cnt.begin(); it != cnt.end(); it++) {
+		int c = it->second;
+		ans += (c * (c - 1)) / 2;
+
+		if (it->first == 0) {
+			ans += it->second;
+		}
+	}
+	return ans;
+}
 
 
 int main() {
