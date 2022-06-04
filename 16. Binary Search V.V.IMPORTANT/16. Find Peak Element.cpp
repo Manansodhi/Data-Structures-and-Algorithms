f04af6456a94e5ed0274b5efe6afff2681e3db11
Manansodhi <sodhimanan022@gmail.com>
2022-06-05 00:02:07 +0530

    Peak Element
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

--- 16. Binary Search V.V.IMPORTANT/16. Find Peak Element.cpp
+++ 16. Binary Search V.V.IMPORTANT/16. Find Peak Element.cpp
@@ -6,7 +6,6 @@ Given an integer array nums, find a peak element, and return its index. If the a
 You may imagine that nums[-1] = nums[n] = -∞.
 You must write an algorithm that runs in O(log n) time.
 
- 
 Example 1:
 Input: nums = [1,2,3,1]
 Output: 2
@@ -21,6 +20,7 @@ Constraints:
 1 <= nums.length <= 1000
 -231 <= nums[i] <= 231 - 1
 nums[i] != nums[i + 1] for all valid i.
+
 */
 
 class Solution {
