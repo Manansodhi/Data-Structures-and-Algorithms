ea8d1f94b08d9a464289ca78718f98a3f33683c9
Manansodhi <sodhimanan022@gmail.com>
2022-06-04 23:56:04 +0530

    Search Insert Position
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

--- 16. Binary Search V.V.IMPORTANT/11. Return Index of Elemnet if it is present if not present then return idx at which it should be present.cpp
+++ 16. Binary Search V.V.IMPORTANT/11. Return Index of Elemnet if it is present if not present then return idx at which it should be present.cpp
@@ -1,5 +1,5 @@
 /*
-35. Search Insert Position
+35. Search Insert Position/11. Return Index of Elemnet if it is present if not present then return idx at which it should be present
 
 Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
 You must write an algorithm with O(log n) runtime complexity.
