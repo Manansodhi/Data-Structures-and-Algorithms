fd948c979f5f8cf33fb0c0086a44559238b55195
Manansodhi <sodhimanan022@gmail.com>
2022-06-27 00:51:54 +0530

    .
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

---
+++ practice/PracticleDP1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// int fibo(int n, vector<int> & dp){
+// 	if(n<=1){//base case
+// 		return n;
+// 	}
+
+// 	if(dp[n]!=-1){//memoization step
+// 		return dp[n];
+// 	}
+
+// 	return dp[n]=fibo(n-1, dp)+fibo(n-2, dp);
+// }
+
+
+
