64899925ab89fc11f4445ccedf78626a45f07f04
Manansodhi <sodhimanan022@gmail.com>
2022-07-08 01:46:01 +0530

    GRAPHS
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

---
+++ 19. Graphs/12. Shortest Path in Undirected Graph with Unit Weights.cpp
@@ -0,0 +1,27 @@
+
+
+void shortestPathUndirectdBFS(vector<int> adj, int n, int src){
+
+	int dist[n];
+	for(int i=0;i<n;i++)
+		dist[i] = INT_MAX;
+	queue<int>q;
+
+	dist[src]=0;
+	q.push(src);
+
+	while(!q.empty()){
+		int node = q.front();
+		q.pop();
+
+		for(auto it:adj[node]){
+			if(dist[node]+1 < dist[it]){
+				dist[it] = dist[node]+1;
+				q.push(it);
+			}
+		}
+	}
+
+	for(int i=0;i<n;i++)
+		cout<<dist[i]<<" ";
+}
