a8585a64ef7a0ea1d2888793bbcd69a86a202fbb
Manansodhi <sodhimanan022@gmail.com>
2022-07-12 18:45:47 +0530

    Dynamic Programming
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

---
+++ 17. Dynamic Programming/56. Maximum Rectangle Area with all 1's(85) (DP on Rectangles).cpp
@@ -0,0 +1,76 @@
+/*
+85. Maximal Rectangle
+
+Given a rows x cols binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's 
+and return its area.
+
+Example 1:
+Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+Output: 6
+Explanation: The maximal rectangle is shown in the above picture.
+
+Example 2:
+Input: matrix = [["0"]]
+Output: 0
+
+Example 3:
+Input: matrix = [["1"]]
+Output: 1
+
+Constraints:
+rows == matrix.length
+cols == matrix[i].length
+1 <= row, cols <= 200
+matrix[i][j] is '0' or '1'.
+*/
+
+
+class Solution {
+public:
+  
+  int largestRectangleArea(vector<int> & histo) {
+      stack < int > st;
+      int maxA = 0;
+      int n = histo.size();
+      for (int i = 0; i <= n; i++) {
+        while (!st.empty() && (i == n || histo[st.top()] >= histo[i])) {
+          int height = histo[st.top()];
+          st.pop();
+          int width;
+          if (st.empty())
+            width = i;
+          else
+            width = i - st.top() - 1;
+          maxA = max(maxA, width * height);
+        }
+        st.push(i);
+      }
+      return maxA;
+    }
+  
+    int maximalRectangle(vector<vector<char>>& mat) {
+      int n=mat.size();
+      if(n==0) return 0;
+      int m=mat[0].size();
+  
+      int maxArea = 0;
+      vector<int>height(m,0);
+      
+      for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(mat[i][j] == '1')
+            height[j]++;
+          else
+            height[j]=0;
+        }
+      int area = largestRectangleArea(height);
+      maxArea=max(maxArea,area);
+    }
+  return maxArea;
+  }
+};
+
+
+    
+
+       
