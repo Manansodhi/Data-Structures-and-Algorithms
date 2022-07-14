0dc738d5337d6afd7a25e65f38abc9451a598915
Manansodhi <sodhimanan022@gmail.com>
2022-07-14 23:44:13 +0530

    TREES
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

---
+++ 20. Trees/03. (DFS)Postorder Traversal of Binary Tree(Recursively).cpp
@@ -0,0 +1,29 @@
+
+struct Node{
+	int data;
+	struct Node* left;
+	struct Node* right;
+	Node(int val){
+		data=val;
+		left=right=NULL;
+	}
+};
+
+
+void postOrder(Node* root){
+	if(root == NULL)
+		return;
+
+	postOrder(root->left);
+	postOrder(root->right);
+	cout<<root->data<", ";
+}
+
+
+int main(){
+	struct Node* root = new node(1);
+	 root->left = new node(2);
+	 root->right = new node(3);
+	 root->left->left = new node(4);
+	 root->left->right = new node(5);
+}
