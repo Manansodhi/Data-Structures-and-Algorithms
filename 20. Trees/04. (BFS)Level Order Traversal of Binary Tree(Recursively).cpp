c7a94484725335e01293497e3edf53d167307d86
Manansodhi <sodhimanan022@gmail.com>
2022-07-15 23:04:30 +0530

    TREES
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

--- 20. Trees/04. (BFS)Level Order Traversal of Binary Tree(Recursively).cpp
+++ 20. Trees/04. (BFS)Level Order Traversal of Binary Tree(Recursively).cpp
@@ -17,7 +17,7 @@ void LevelOrder(Node* root){
 	queue<Node*> q;
 	q.push(root);
 	while(!q.empty()){
-		int size=q.aize();
+		int size=q.size();
 		vector<int>level;
 		for(int i=0;i<size;i++){
 			Node* node=q.front();
