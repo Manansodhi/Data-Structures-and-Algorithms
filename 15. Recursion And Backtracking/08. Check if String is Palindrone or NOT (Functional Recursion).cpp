1bf1b6716601c7d4068e7ff43b97a41f03c07a10
Manansodhi <sodhimanan022@gmail.com>
2022-08-11 03:16:46 +0530

    RECURSION
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

---
+++ 15. Recursion And Backtracking/08. Check if String is Palindrone or NOT (Functional Recursion).cpp
@@ -0,0 +1,18 @@
+
+// Fumctional Recursion
+// T.C --> O(N/2)
+// S.C --> O(N/2)
+
+int palindrone(int i, string s){
+	int n=s.length();
+	if(i>=n/2)
+		return true;
+
+	if(s[i] != s[n-i-1])
+		return false;
+	return palindrone(i+1);
+}
+
+int main(){
+	return palindrone(0,str);
+}
