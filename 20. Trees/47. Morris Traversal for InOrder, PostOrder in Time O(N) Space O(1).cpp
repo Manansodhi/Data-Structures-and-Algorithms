76ce82500cf8a65291288d88255f8267a20641e3
Manansodhi <sodhimanan022@gmail.com>
2022-08-15 23:43:51 +0530

    TREES
    Signed-off-by: Manansodhi <sodhimanan022@gmail.com>

---
+++ 20. Trees/47. Morris Traversal for InOrder, PostOrder in Time O(N) Space O(1).cpp
@@ -0,0 +1,59 @@
+
+//INORDER
+vector<int> getInorder(TreeNode* root){
+	vector<int> inorder;
+	TreeNode *curr = root;
+	while(curr != NULL){
+		if(curr->left == NULL){
+			inorder.push_back(curr->val);
+			curr = curr->right;
+		} 
+		else{
+			TreeNode *prev = curr->left;
+			while(prev->right && prev->right != curr){
+				prev = prev->right;
+			}
+
+			if(prev->right == NULL){
+				prev->right = curr;
+				curr = curr->left;
+			}
+			else{
+				prev->right = NULL;
+				inorder.push_back(curr->val);
+				curr = curr->right;
+			}
+		}
+	}
+	return inorder;
+}
+
+
+//PREORDER
+vector<int> getInorder(TreeNode* root){
+	vector<int> preorder;
+	TreeNode *curr = root;
+	while(curr != NULL){
+		if(curr->left == NULL){
+			preorder.push_back(curr->val);
+			curr = curr->right;
+		} 
+		else{
+			TreeNode *prev = curr->left;
+			while(prev->right && prev->right != curr){
+				prev = prev->right;
+			}
+
+			if(prev->right == NULL){
+				prev->right = curr;
+				preorder.push_back(curr->val);
+				curr = curr->left;
+			}
+			else{
+				prev->right = NULL;
+				curr = curr->right;
+			}
+		}
+	}
+	return preorder;
+}
